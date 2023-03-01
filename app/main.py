bd5690c06247940fc4e1671f394dcb8110c54a9d
Jose Andres Trujillo <trujillohenaresj@gmail.com>
2023-03-01 19:59:49 +0100

    cambios para plantillas

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="Ai2SQL")
+app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
+templates = Jinja2Templates(directory="frontend")
 
-
-@app.get("/")
-def read_root():
-    return {"hello": "world"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request
+    })
