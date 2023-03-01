934dff6e1f4c2e956b73cc9276a5b73cdda7b835
JoaquinMateosBarroso <jmateosbarroso@gmail.com>
2023-03-01 18:51:31 +0100

    frontend

---
+++ frontend/script.js
@@ -0,0 +1,32 @@
+
+var boton = document.getElementById("mic");
+boton.onclick = function() {
+
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+        
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+    recognition.lang = "es-ES";
+
+    let p = document.createElement('p');
+    const words = document.querySelector('#text');
+    words.appendChild(p);
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+
+        p.textContent = transcript;
+        if(e.results[0].isFinal) {
+            words.innerHTML(p);
+        }
+    });
+
+    recognition.addEventListener('end', recognition.start);
+
+    recognition.start();
+}
