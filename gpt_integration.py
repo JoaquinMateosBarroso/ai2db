1bb06f9dc0730b481d2a80bf2ab828c10a19597d
JoaquinMateosBarroso <jmateosbarroso@gmail.com>
2023-03-01 17:42:25 +0100

    setup

--- gpt_integration.py
+++ gpt_integration.py
@@ -1,7 +1,7 @@
 import openai
 
 def setupfor_sql():
-    openai.api_key = "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"
+    openai.api_key = input("Introduce tu key de GPT, que se obtiene en la página de OpenAI\n")
     prompt = "Tengo una base de datos SQL en la que hay una tabla \"usuarios\" con los atributos \"nombre\" y \"dni\", y hay una tabla \"datos\" con los atributos \"dni\"(clave foránea de usuarios) y \"direccion\". De ahora en adelante respóndeme únicamente con las sentencias que resuelven las consultas que te voy a pedir."
     completion = openai.Completion.create(engine="text-davinci-002",
                                           prompt=prompt,
