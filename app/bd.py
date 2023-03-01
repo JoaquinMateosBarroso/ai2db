6318baf615c54ff74ea7c690eedf342d9443cafa
Jose Andres Trujillo <trujillohenaresj@gmail.com>
2023-03-01 18:58:17 +0100

    queries

--- app/bd.py
+++ app/bd.py
@@ -5,25 +5,19 @@ from config import config
 def connect():
     conn = None
     try:
-        # Leemos los parámetros de conexión
         params = config()
 
-        # Conectamos con el servidor PostgreSQL
         print('Conectando con PostgreSQL...')
         conn = psycopg2.connect(**params)
 
-        # creamos un cursor
         cur = conn.cursor()
 
-        # Ejecutamos una sentencia SQL
         print('PostgreSQL database version:')
         cur.execute('SELECT version()')
 
-        # Mostramos la versión de PostgreSQL que hemos solicitado con la sentencia anterior
         db_version = cur.fetchone()
         print(db_version)
 
-        # Cerramos la comunicación con PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -32,6 +26,29 @@ def connect():
             conn.close()
             print('Database connection closed.')
 
+
+def query(query: str):
+    sql = """{query}"""
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        cur.execute(sql)
+
+        result = []
+        row = cur.fetchone()
+        while row is not None:
+            result.append(row)
+            row = cur.fetchone()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return result
 
 if __name__ == '__main__':
     connect()
