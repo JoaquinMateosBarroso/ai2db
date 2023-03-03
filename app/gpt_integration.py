88e80c5144e017d9265141bce2c7ebd22ac27f19
JoaquinMateosBarroso <jmateosbarroso@gmail.com>
2023-03-03 17:38:47 +0100

    things

--- app/gpt_integration.py
+++ app/gpt_integration.py
@@ -1,18 +1,19 @@
 import openai
 
-def setupfor_sql():
-    openai.api_key = input("Introduce tu key de GPT, que se obtiene en la página de OpenAI\n")
-    prompt = "Tengo una base de datos SQL en la que hay una tabla \"usuarios\" con los atributos \"nombre\" y \"dni\", y hay una tabla \"datos\" con los atributos \"dni\"(clave foránea de usuarios) y \"direccion\". De ahora en adelante respóndeme únicamente con las sentencias que resuelven las consultas que te voy a pedir."
+def setupfor_sql(key: str):
+    openai.api_key = key
+    prompt = "PONER BIEN ESTO"
     completion = openai.Completion.create(engine="text-davinci-002",
                                           prompt=prompt,
                                           max_tokens=100)
     return completion
 
-def pediryresponder_sql(text_for_query: str):
-    setupfor_sql()
+
+def pediryresponder_sql(text_for_query: str, key: str):
+    setupfor_sql(key)
     
     completion = openai.Completion.create(engine="text-davinci-002",
                                           prompt=text_for_query,
                                           max_tokens=100)
+    
     return completion.choices[0].text
-
