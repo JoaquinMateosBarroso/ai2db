ec5d2ca4f650d1afb2e0207bc3b1fb553a87d2e4
JoaquinMateosBarroso <jmateosbarroso@gmail.com>
2023-03-03 17:34:38 +0100

    things

--- app/bd.py
+++ app/bd.py
@@ -51,56 +51,56 @@ def query(query: str):
     return result
 
 
-def create_table(creacion: str):
-    """ create tables in the PostgreSQL database"""
+# def create_table(creacion: str):
+#     """ create tables in the PostgreSQL database"""
     
-    conn = None
-    try:
-        # read the connection parameters
-        params = config()
-        # connect to the PostgreSQL server
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        # create table 
-        cur.execute(creacion)
-        # close communication with the PostgreSQL database server
-        cur.close()
-        # commit the changes
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+#     conn = None
+#     try:
+#         # read the connection parameters
+#         params = config()
+#         # connect to the PostgreSQL server
+#         conn = psycopg2.connect(**params)
+#         cur = conn.cursor()
+#         # create table 
+#         cur.execute(creacion)
+#         # close communication with the PostgreSQL database server
+#         cur.close()
+#         # commit the changes
+#         conn.commit()
+#     except (Exception, psycopg2.DatabaseError) as error:
+#         print(error)
+#     finally:
+#         if conn is not None:
+#             conn.close()
 
     
     
     
-def insert_vendor(sql: str):
-    conn = None
-    vendor_id = None
-    try:
-        # read database configuration
-        params = config()
-        # connect to the PostgreSQL database
-        conn = psycopg2.connect(**params)
-        # create a new cursor
-        cur = conn.cursor()
-        # execute the INSERT statement
-        cur.execute(sql, "ai2sql")
-        # get the generated id back
-        vendor_id = cur.fetchone()[0]
-        # commit the changes to the database
-        conn.commit()
-        # close communication with the database
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+# def insert_vendor(sql: str):
+#     conn = None
+#     vendor_id = None
+#     try:
+#         # read database configuration
+#         params = config()
+#         # connect to the PostgreSQL database
+#         conn = psycopg2.connect(**params)
+#         # create a new cursor
+#         cur = conn.cursor()
+#         # execute the INSERT statement
+#         cur.execute(sql, "ai2sql")
+#         # get the generated id back
+#         vendor_id = cur.fetchone()[0]
+#         # commit the changes to the database
+#         conn.commit()
+#         # close communication with the database
+#         cur.close()
+#     except (Exception, psycopg2.DatabaseError) as error:
+#         print(error)
+#     finally:
+#         if conn is not None:
+#             conn.close()
 
-    return vendor_id
+#     return vendor_id
 
 
 
