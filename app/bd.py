4e88548e9ff1137b265637bcb7dff0b77fb8db5d
JoaquinMateosBarroso <jmateosbarroso@gmail.com>
2023-03-03 16:51:27 +0100

    insert code

--- app/bd.py
+++ app/bd.py
@@ -1,5 +1,5 @@
 import psycopg2
-from config import config
+from app.config import config
 
 
 def connect():
@@ -49,6 +49,60 @@ def query(query: str):
         if conn is not None:
             conn.close()
     return result
+
+
+def create_table(creacion: str):
+    """ create tables in the PostgreSQL database"""
+    
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table 
+        cur.execute(creacion)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    
+    
+    
+def insert_vendor(sql: str):
+    conn = None
+    vendor_id = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, "ai2sql")
+        # get the generated id back
+        vendor_id = cur.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return vendor_id
+
+
 
 if __name__ == '__main__':
     connect()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from app.bd import create_table, insert_vendor, query
+
 app = FastAPI(title="Ai2SQL")
 app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
 templates = Jinja2Templates(directory="frontend")
+
 
 @app.get("/", response_class=HTMLResponse)
 async def main( request: Request, prompt: str = ""):
@@ -19,3 +22,42 @@ async def main( request: Request, prompt: str = ""):
             "table": [prompt]
         })
     return response
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    
+    contents = await file.read()
+    rows = contents.decode("utf-8").split("\n")
+    
+    input_para_chatgpt = rows[0].split(",")
+    rows[0] = [row + " VARCHAR2(255)" for row in input_para_chatgpt]
+    
+    nombre_tabla = file.filename.split(".")[0]
+    creacion = "CREATE TABLE " + nombre_tabla + " (" + ", ".join(rows[0]) + ");"
+    
+    
+    try:
+        create_table(creacion)
+    except Exception as error:
+        raise error
+
+    for row in rows[1:]:
+        sentencia = "INSERT INTO " + nombre_tabla + " VALUES (" + ", ".join(["'" + r + "'" for r in row.split(",")]) + ");"
+        try:
+            insert_vendor(sentencia)
+        except Exception as error:
+            raise error
+    
+    return {"tablename": nombre_tabla}
+
+
+@app.get("/table/{tablename}", response_class=HTMLResponse)
+async def read_table(tablename: str):
+    
+    try:
+        tabla = query("SELECT * FROM " + tablename)
+    except Exception as error:
+        raise error
+    
+    return {"table": tabla}
